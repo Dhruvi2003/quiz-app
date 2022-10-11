dfdb5fe59f83db73ed832edd7f34a343563ca24a
Dhruvi2003 <78583125+Dhruvi2003@users.noreply.github.com>
2022-10-12 01:29:19 +0530

    Quiz App

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is used to read an HTML page and render it?",
+    answer: " Web browser",
+    options: [
+      "Web server",
+     "Web network",
+     "Web browser",
+      "Web matrix"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In which part of the HTML metadata is contained?",
+    answer: "head tag",
+    options: [
+      "head tag",
+      "title tag",
+      "html tag",
+       "body tag"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following elements in HTML5 defines video or movie content?",
+    answer: "video",
+    options: [
+      "video",
+      "movie",
+      "audio",
+      "media"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which attribute is not essential under iframe?",
+    answer: "frameborder",
+    options: [
+      "frameborder",
+      "width",
+      "height",
+      "src"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which attribute specifies a unique alphanumeric identifier to be associated with an element?",
+    answer: "id",
+    options: [
+      "type",
+"article",
+"id",
+"class"
+    ]
+  },
+];
